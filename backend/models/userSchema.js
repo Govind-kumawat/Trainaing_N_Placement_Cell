0942953e1f6c4d0fc601f305c99ae382e50513e0
Govind-kumawat <80705927+Govind-kumawat@users.noreply.github.com>
2024-02-22 14:29:29 +0530

    first commit

---
+++ backend/models/userSchema.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide full name!"],
+        minLength: [3, "Name must contain 3 character!"],
+        maxLenght: [30, "Name cannot exceed 30 characters!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide your email!"],
+        validate: [validator.isEmail, "Please provide valid email!"]
+    },
+    phone: {
+        type: Number,
+        required: [true, "Please provide phone number!"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password!"],
+        minLength: [8, "Password must be minimum 8 characters"],
+        maxLenght: [15, "Password cannot be more than 15 characters"]
+    },
+    role: {
+        type: String,
+        required: [true, "Please provide your role"],
+        enum: ["Job Seeker", "Employee"]
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Hashing the password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+//Comparing the password
+userSchema.methods.comparePassword = async (enteredPassword) => {
+    await bcrypt.compare(enteredPassword, this.password);
+}
+
+//Generating jwt token
+userSchema.methods.getJwtToken = () => {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRE });
+}
+
+export const User = mongoose.model("User", userSchema);
